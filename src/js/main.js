e78d2d5e73075c8512621b47ae1197eaf48e9165
sali1502 <sali1502@student.miun.se>
2024-03-17 21:28:24 +0100

    Diagram utan data inlästa

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import Chart from 'chart.js/auto';
+
 // Hämta in meny-knapparna
 let openBtn = document.getElementById("open-menu");
 let closeBtn = document.getElementById("close-menu");
@@ -22,5 +24,38 @@ function toggleMenu() {
         navMenuEl.style.display = "none";
     }
 }
+
+// Diagran - stapel
+const barChartEl = document.getElementById('barChart');
+
+new Chart(barChartEl, {
+    type: 'bar',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [{
+            label: 'Antal sökande HT2023',
+            data: [12, 19, 3, 5, 2, 3,],
+            borderWidth: 1
+        }]
+    },
+});
+
+// Diagran - cirkel
+const pieChartEl = document.getElementById('pieChart');
+
+
+new Chart(pieChartEl, {
+    type: 'pie',
+    data: {
+        labels: ['1', '2', '3', '4', '5'],
+        datasets: [{
+            label: 'Antal sökande HT2023',
+            data: [12, 19, 3, 5, 2,],
+            borderWidth: 1
+        }]
+    },
+});
+
+
 
 
